a9921210f0944f0d99cd062cc5d17be309dbee0e
Kamil Janas <127615923+Yaynas@users.noreply.github.com>
2023-03-12 12:33:28 +0100

    Study-120323

--- ToDoList.Core/ViewModels/Controls/WorkTaskViewModel.cs
+++ ToDoList.Core/ViewModels/Controls/WorkTaskViewModel.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToDoList.Core;
+using ToDoList.Core.ViewModels.BaseViewModel;
 
 namespace ToDoList.Core
 {
-    public class WorkTaskViewModel
+    public class WorkTaskViewModel : BaseViewModel
     {
+        public bool IsSelected { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
--- ToDoList.Core/ViewModels/Pages/WorkTaskPageViewModel.cs
+++ ToDoList.Core/ViewModels/Pages/WorkTaskPageViewModel.cs
@@ -1,35 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ToDoList.Core;
+using ToDoList.Core.ViewModels.BaseViewModel;
 
 namespace ToDoList.Core;
 
-public class WorkTaskPageViewModel
+public class WorkTaskPageViewModel : BaseViewModel
 {
     public ObservableCollection<WorkTaskViewModel> WorkTasksList { get; set; } = new ObservableCollection<WorkTaskViewModel>();
 
     public string NewWorkTaskTitle { get; set; }
     public string NewWorkTaskDescription { get; set; }
     public ICommand AddNewTaskCommand { get; set; }
+    public ICommand DeleteSelectedTaskCommand { get; set; }
 
     public WorkTaskPageViewModel()
-    {
+    {        
         AddNewTaskCommand = new RelayCommand(AddNewTask);
+        DeleteSelectedTaskCommand = new RelayCommand(DeleteSelectedTask);
     }
     private void AddNewTask()
     {
-        var NewTask = new WorkTaskViewModel
+        if(!String.IsNullOrEmpty(NewWorkTaskTitle))
         {
-            Title = NewWorkTaskTitle,
-            Description = NewWorkTaskDescription,
-            CreatedDate = DateTime.Now
-        };
+            var NewTask = new WorkTaskViewModel
+            {
+                Title = NewWorkTaskTitle,
+                Description = NewWorkTaskDescription,
+                CreatedDate = DateTime.Now
+            };
 
-        WorkTasksList.Add(NewTask);
+            WorkTasksList.Add(NewTask);
+
+            NewWorkTaskTitle = string.Empty;
+            NewWorkTaskDescription = string.Empty;
+        }
+                 
+    }
+
+    private void DeleteSelectedTask()
+    {
+        var selectedTasks = WorkTasksList.Where(x => x.IsSelected).ToList();
+        foreach (var task in selectedTasks)
+        {
+            WorkTasksList.Remove(task);
+        }
     }
 }
